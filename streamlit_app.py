2a30aa914b84e3292b4ca3405f2811d386669e57
Debasish Tudu <65746343+devsTudu@users.noreply.github.com>
2025-01-29 12:27:46 +0000

    Initial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from models.gemini import GeminiAssist
 
-st.title("🎈 My new app")
+
+st.title("Quick Edit Resume")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's give the best for every opportunity"
 )
+
+with st.sidebar:
+    gemini_key = st.text_input("Gemini API Key", key="gemini_api_key", type="password")
+    "[Get an Gemini API key](https://aistudio.google.com/app/apikey)"
+
+    open_ai_key = st.text_input("OpenAI API Key",type="password")
+    "[Get OpenAI API Key](https://platform.openai.com/api-keys)"
